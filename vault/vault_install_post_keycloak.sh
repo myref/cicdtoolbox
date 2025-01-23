ff414bbae5ff8780dd78f63924ce48ecda6be976
Mark Sibering <mark.sibering@devoteam.com>
2025-01-23 11:49:42 +0100

    tf fix

--- vault/vault_install_post_keycloak.sh
+++ vault/vault_install_post_keycloak.sh
@@ -28,5 +28,13 @@ export VAULT_ADDR=https://vault.internal.${DOMAIN_NAME_SL}.${DOMAIN_NAME_TL}:820
 export VAULT_TOKEN=$(cat vault/token.txt)
 export TF_VAR_client_secret=$(grep VAULT_token install_log/keycloak_create.log | cut -d' ' -f2 | tr -d '\r' )
 export TF_VAR_client_id="Vault"
-terraform -chdir=terraform/vault init -input=false
+echo "****************************************************************************************************************"
+echo " Creating admin policy file with correct domain name" 
+echo "****************************************************************************************************************"
+rm terraform/vault/variables.tf
+cp terraform/vault/variables.tf.org terraform/vault/variables.tf
+sed -i "s/\${DOMAIN_NAME_TL}/${DOMAIN_NAME_TL}/" terraform/vault/variables.tf
+sed -i "s/\${DOMAIN_NAME_SL}/${DOMAIN_NAME_SL}/" terraform/vault/variables.tf
+
+terraform -chdir=terraform/vault init -input=false -e DOMAIN_NAME_SL=${DOMAIN_NAME_SL} -e DOMAIN_NAME_TL=${DOMAIN_NAME_TL}
 terraform -chdir=terraform/vault apply --auto-approve
