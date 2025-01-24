c985a321730e694d27fb09e2a9c65f3a0ae83a21
Mark Sibering <mark.sibering@devoteam.com>
2025-01-24 11:07:34 +0100

    fix

--- cicdtoolbox-db/create-multiple-postgresql-databases.sh
+++ cicdtoolbox-db/create-multiple-postgresql-databases.sh
@@ -7,7 +7,7 @@ function create_user_and_database() {
 	local database=$1
 	local user=$2
 	local password=$3
-	echo "  Creating user and database '$database'"
+	echo "  Creating user '$user' with password '$password' and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	    CREATE USER $user WITH PASSWORD '$password';
 	    CREATE DATABASE $database OWNER $user ;
