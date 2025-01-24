2a9fd0ff95c0d758ba45e93b6e53dc730f1698ae
Mark Sibering <mark.sibering@devoteam.com>
2025-01-24 10:45:04 +0100

    fix db creation to use set username and password

--- cicdtoolbox-db/create-multiple-postgresql-databases.sh
+++ cicdtoolbox-db/create-multiple-postgresql-databases.sh
@@ -9,9 +9,9 @@ function create_user_and_database() {
 	local password=$3
 	echo "  Creating user and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database WITH PASSWORD '$password';
+	    CREATE USER $user WITH PASSWORD '$password';
 	    CREATE DATABASE $database OWNER $user ;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $user;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
 }
@@ -19,7 +19,9 @@ EOSQL
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-		create_user_and_database $db $db $db	
+		local dbuser=$(echo ${db}"_db_user")
+		local dbpwd=$(echo ${db}"_db_pwd")
+		create_user_and_database $db $dbuser $dbpwd	
 	done
 	echo "Multiple databases created"
 fi
