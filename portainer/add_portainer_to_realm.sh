ed198149e211b24c8b38a4ac7098435ad3e652a1
Mark Sibering <mark.sibering@devoteam.com>
2025-02-20 00:33:21 +0100

    working with portainer

--- portainer/add_portainer_to_realm.sh
+++ portainer/add_portainer_to_realm.sh
@@ -8,45 +8,53 @@ cd $HOME/bin
 echo "Credentials created"
 
 #Add Portainer client
-./kcadm.sh create clients \
+PORTAINER_ID=$(./kcadm.sh create clients \
     -r cicdtoolbox \
     -s name="Portainer" \
     -s description="System to manage containers in the toolchain" \
     -s clientId=Portainer \
     -s enabled=true \
-    -s publicClient=false \
-    -s serviceAccountsEnabled=true \
-    -s fullScopeAllowed=false \
+    -s clientAuthenticatorType="client-secret" \
+    -s 'redirectUris=[ "https://portainer.monitoring.provider.test:9443*" ]' \
+    -s 'webOrigins=[ "https://portainer.monitoring.provider.test:9443/" ]' \
     -s standardFlowEnabled=true \
-    -s frontchannelLogout=true \
     -s directAccessGrantsEnabled=true \
-    -s rootUrl=http://portainer.monitoring.provider.test:9000 \
-    -s adminUrl=http://portainer.monitoring.provider.test:9000/ \
-    -s 'redirectUris=[ "http://portainer.monitoring.provider.test:9000/*" ]' \
-    -s 'webOrigins=[ "http://portainer.monitoring.provider.test:9000/" ]' \
-    -o --fields id >cicdtoolbox_PORTAINER
+    -s serviceAccountsEnabled=false \
+    -s publicClient=false \
+    -s frontchannelLogout=true \
+    -s protocol="openid-connect" \
+    -s attributes="{ \
+        \"realm_client\": \"false\", \
+        \"oidc.ciba.grant.enabled\": \"false\", \
+        \"frontchannel.logout.session.required\": \"true\", \
+        \"post.logout.redirect.uris\": \"https://portainer.monitoring.provider.test:9443*\", \
+        \"display.on.consent.screen\": "false", \
+        \"oauth2.device.authorization.grant.enabled\": \"true\" \
+       }" \
+    -s fullScopeAllowed=false \
+    -s rootUrl=https://portainer.monitoring.provider.test:9443 \
+    -s adminUrl=https://portainer.monitoring.provider.test:9443/ \
+    -o --fields id | grep id | cut -d'"' -f 4)
 
-# output is Created new client with id, we now need to grep the ID out of it
-PORTAINER_ID=$(cat cicdtoolbox_PORTAINER | grep id | cut -d'"' -f 4)
+echo "Created Portainer client with ID: ${PORTAINER_ID}" 
 
 # Create Client secret
 ./kcadm.sh create clients/$PORTAINER_ID/client-secret -r cicdtoolbox
 
 # We need to retrieve the token from keycloak for this client
-./kcadm.sh get clients/$PORTAINER_ID/client-secret -r cicdtoolbox >cicdtoolbox_portainer_secret
-PORTAINER_token=$(grep value cicdtoolbox_portainer_secret | cut -d '"' -f4)
+PORTAINER_token=$(./kcadm.sh get clients/$PORTAINER_ID/client-secret -r cicdtoolbox | grep value | cut -d '"' -f4)
 
 # Make sure we can grep the clienttoken easily from the keycloak_create.log to create an authentication source in Portainer for Keycloak
 echo "PORTAINER_token: ${PORTAINER_token}"
 
 # Now we can add client specific roles (Clientroles)
-./kcadm.sh create clients/$PORTAINER_ID/roles -r cicdtoolbox -s name=PORTAINER-admin -s description='The admin role for Portainer'
+./kcadm.sh create clients/$PORTAINER_ID/roles -r cicdtoolbox -s name=local-admin -s description='The admin role for Portainer'
 echo "Portainer configuration finished"
 
 # We need to add the portainer-admin claim and portainer-group claim to the token
 ./kcadm.sh create clients/$PORTAINER_ID/protocol-mappers/models \
     -r cicdtoolbox \
-	-s name=group-mapper \
+	-s name=portainer-group-mapper \
     -s protocol=openid-connect \
 	-s protocolMapper=oidc-usermodel-client-role-mapper \
     -s consentRequired=false \
@@ -54,10 +62,9 @@ echo "Portainer configuration finished"
 
 echo "Created role-group mapper in the Client Scope" 
 
-# # We need to add a client scope on the realm for Portainer in order to include the audience in the access token
-# ./kcadm.sh create -x "client-scopes" -r cicdtoolbox -s name=portainer-audience -s protocol=openid-connect &>cicdtoolbox_PORTAINER_SCOPE
-# PORTAINER_SCOPE_ID=$(cat cicdtoolbox_PORTAINER_SCOPE | grep id | cut -d"'" -f 2)
-# echo "Created Client scope for Portainer with id: ${PORTAINER_SCOPE_ID}" 
+# We need to add a client scope on the realm for Portainer in order to include the audience in the access token
+PORTAINER_SCOPE_ID=$(./kcadm.sh create -x "client-scopes" -r cicdtoolbox -s name=portainer-audience -s protocol=openid-connect | grep id | cut -d"'" -f 2)
+echo "Created Client scope for Portainer with id: ${PORTAINER_SCOPE_ID}" 
 
 # Create a mapper for the audience
 ./kcadm.sh create clients/$PORTAINER_ID/protocol-mappers/models \
@@ -68,5 +75,14 @@ echo "Created role-group mapper in the Client Scope"
     -s consentRequired=false \
 	-s config="{\"included.client.audience\" : \"https://keycloak.services.${DOMAIN_NAME_SL}.${DOMAIN_NAME_TL}:8443/realms/cicdtoolbox\",\"id.token.claim\" : \"false\",\"access.token.claim\" : \"true\"}"
 
-# echo "Created audience mapper in the Client Scope" 
+echo "Created audience mapper in the Client Scope" 
 
+toolbox_admin_id=$(cat TOOLBOX_ADMIN | grep id | cut -d"'" -f 2)
+echo "Retrieved Toolbox Admins group ID: ${toolbox_admin_id}" 
+
+#adding client roles to the group
+./kcadm.sh add-roles \
+    -r cicdtoolbox \
+    --gid $toolbox_admin_id \
+    --cclientid Portainer \
+    --rolename local-admin 
---
+++ portainer/configure_portainer.robot
@@ -0,0 +1,60 @@
+*** Settings ***
+Library                               SeleniumLibrary
+Library                               OperatingSystem
+Library                               XML
+Documentation                         Setting up Portainer
+
+Suite Setup       Open Browser   ${URL}      ${BROWSER1}       remote_url=http://seleniumgchost.internal.%{DOMAIN_NAME_SL}.%{DOMAIN_NAME_TL}:4444    options=add_argument("--ignore-certificate-errors")
+Suite Teardown    Close Browser
+
+*** Test cases ***
+Set local-admin name 
+    Set Admin credentials
+    
+# Configure Portainer
+#     Choose environment
+
+*** Variables ***
+
+${BROWSER1}                            chrome
+${DELAY}                               0
+${URL}                                 http://portainer.monitoring.%{DOMAIN_NAME_SL}.%{DOMAIN_NAME_TL}:9000
+${SSL_URL}                             https://portainer.monitoring.%{DOMAIN_NAME_SL}.%{DOMAIN_NAME_TL}:9443
+${KEYCLOAK_URL}                        https://keycloak.services.%{DOMAIN_NAME_SL}.%{DOMAIN_NAME_TL}:8443
+${PORTAINER_ADMIN}                     %{local_admin_user}
+${PORTAINER_ADMIN_PASSWORD}            %{local_admin_password}
+
+*** Keywords ***
+Set Admin credentials
+    Wait Until Page Contains           New Portainer installation
+    Input Text                         username                ${PORTAINER_ADMIN}
+    Input Text                         password                ${PORTAINER_ADMIN_PASSWORD}${PORTAINER_ADMIN_PASSWORD}
+    Input Text                         confirm_password        ${PORTAINER_ADMIN_PASSWORD}${PORTAINER_ADMIN_PASSWORD}
+    Click Element                      toggle_enableTelemetry
+    Click Element                      xpath://*[text()='Create user']
+
+Choose environment
+    Wait Until Page Contains           Environment Wizard
+    Go To                              ${URL}/#!/settings/auth
+    Click Element                      xpath://*[text()='OAuth']
+    Scroll Element Into View           oauth_client_id
+    Input Text                         oauth_client_id            Portainer
+    Scroll Element Into View           oauth_client_secret
+    Input Text                         oauth_client_secret        %{portainer_secret}
+    Scroll Element Into View           oauth_authorization_uri
+    Input Text                         oauth_authorization_uri    ${KEYCLOAK_URL}/realms/cicdtoolbox/protocol/openid-connect/auth
+    Scroll Element Into View           oauth_access_token_uri
+    Input Text                         oauth_access_token_uri     ${KEYCLOAK_URL}/realms/cicdtoolbox/protocol/openid-connect/token
+    Scroll Element Into View           oauth_resource_uri
+    Input Text                         oauth_resource_uri         ${KEYCLOAK_URL}/realms/cicdtoolbox/protocol/openid-connect/userinfo
+    Scroll Element Into View           oauth_redirect_uri
+    Input Text                         oauth_redirect_uri         ${SSL_URL}
+    Scroll Element Into View           oauth_logout_url
+    Input Text                         oauth_logout_url           ${KEYCLOAK_URL}/realms/cicdtoolbox/protocol/openid-connect/logout
+    Scroll Element Into View           oauth_user_identifier
+    Input Text                         oauth_user_identifier      email
+    Scroll Element Into View           oauth_scopes
+    Input Text                         oauth_scopes               openid
+    Scroll Element Into View           xpath://*[text()='Save settings']
+    Click Element                      xpath://*[text()='Save settings']
+
